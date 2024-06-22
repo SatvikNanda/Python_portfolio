3ca8b047cb42a8455638235be65f1ac212705f4a
satvik nanda <satvik.nanda@gmail.com>
2024-06-22 17:07:38 +0530

    sending email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.header("Contact me")
+
+with st.form(key = "email_forms"):
+    user_email = st.text_input("You email address: ")
+    message = st.text_area("Enter your message")
+
+    button = st.form_submit_button("Submit")
