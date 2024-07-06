7f3995221fe3a42064de8778a93b2fd2c241d139
satvik nanda <satvik.nanda@gmail.com>
2024-07-06 18:18:22 +0530

    modifications

--- Home.py
+++ Home.py
@@ -6,12 +6,17 @@ st.set_page_config(layout = "wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images(1)/photo.png")
+    st.image("images(1)/Screenshot_20240705_160315.jpg", width=400)
 
 with col2:
     st.title("Satvik Nanda")
     content = """
-    Hi, i am satvik.
+    Hello! I'm Satvik Nanda, currently entering my final year of my Computer Science degree at the Vellore Institute of Technology, Vellore. When I'm not deciphering complex algorithms or debugging my code (or my life), you’ll find me scratching my head around Python.
+
+I picked up a keen interest for Python during the last six months which has encouraged me to manuever my way around projects, helping me improve my ability on every step. This website is a testament to my passion, showcasing the projects I've crafted.
+
+From automating mundane tasks to developing intricate web applications, Python has been my go-to tool. I firmly believe in the mantra: “Code, Debug, Coffee, Repeat,” and I live by it every day.
+
     """
     st.info(content)
 
--- send_email.py
+++ send_email.py
@@ -1,22 +1,19 @@
 import smtplib, ssl
+import os
 
-host = "smtp.gmail.com"
-port = 465
-
-
-username = "satvik.nanda@gmail.com"
-password = "dptw hatq vaqc algt"
-receiver = "satvik.nanda@gmail.com"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
 
-context  = ssl.create_default_context()
-message = """\
-Subject: Hi.
-How are you?
-Bye!
- """
+    username = "satvik.nanda@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "satvik.nanda@gmail.com"
 
-with smtplib.SMTP_SSL(host, port, context = context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+
+    context  = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context = context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
