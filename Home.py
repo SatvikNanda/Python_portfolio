2ea75893f07bb04d2b954e1564f3bb826455a279
satvik nanda <satvik.nanda@gmail.com>
2024-08-06 13:58:00 +0530

    updated portfolio

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ st.set_page_config(layout = "wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images(1)/Screenshot_20240705_160315.jpg", width=400)
+    st.image("images(1)/21.jpg", width=400)
 
 with col2:
     st.title("Satvik Nanda")
@@ -35,14 +35,14 @@ col3, empty_col, col4 = st.columns([3,1,3])
 df = pd.read_csv("data.csv", sep=";")
 
 with col3:
-    for index, row in df[:10].iterrows():
+    for index, row in df[:4].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images(1)/" + row["image"])
         st.write(f"[Source code]({row['url']})")
 
 with col4:
-    for index, row in df[10:].iterrows():
+    for index, row in df[4:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images(1)/" + row["image"])
